651a19b0da734bcc89e36a3729d7e39cb524686e
Shazzadul-Shakib <shakib1186@gmail.com>
2023-11-22 12:46:15 +0600

    add react scroll to smoth scroll to the nav sections

---
+++ app/skills/page.tsx
@@ -0,0 +1,23 @@
+export default function Skills() {
+  return (
+    <main id="skills">
+      <div className="w-[90%] h-screen mx-auto p-4 my-10 bg-gray-400 rounded-md ">
+        <h1>Skill</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+          mollitia quis quia explicabo consequuntur vero neque ipsum nisi
+          dignissimos repellat nam sunt reiciendis nesciunt ducimus facilis
+          delectus facere, quidem repellendus, reprehenderit quaerat illo
+          numquam. Tempora deleniti error rem dolorum minima consequatur libero
+          sint at dicta, vero delectus nemo consequuntur ducimus dignissimos
+          quod laudantium officia explicabo autem. Similique qui impedit
+          architecto fugiat mollitia facere vel commodi quia temporibus.
+          Delectus nostrum quia maiores illo esse, reiciendis possimus cumque
+          architecto accusantium. Placeat officiis quas in delectus consectetur
+          quae quisquam qui, minus non dolorum iste magni modi est eveniet
+          voluptatem, sit at error saepe!
+        </p>
+      </div>
+    </main>
+  );
+}
