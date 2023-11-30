64942b2fbbf41cd23aa9cc51849fde4abd6d15df
Shazzadul-Shakib <shakib1186@gmail.com>
2023-11-30 18:33:26 +0600

    template of skills added

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,8 +1,33 @@
 export default function Skills() {
   return (
     <main id="skills">
-      <div className="w-[90%] h-screen mx-auto p-4 my-10 bg-gray-400 rounded-md ">
-        <h1>Skill</h1>
+      <div className="w-[90%] h-screen mx-auto p-4 my-10 rounded-md text-white ">
+        <h1 className="text-center text-2xl font-bold">Skills</h1>
+        <section className=" w-[80%] mx-auto">
+          <div className="border rounded-md m-3 p-4 ">
+            <h1 className="text-xl text-center">Front-end</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+              ab!
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <div className="border rounded-md m-3 p-4 ">
+              <h1 className="text-xl text-center">Back-end</h1>
+              <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+              ab!
+            </p>
+            </div>
+            <div className="border rounded-md m-3 p-4 ">
+              <h1 className="text-xl text-center">Tools</h1>
+              <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+              ab!
+            </p>
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   );
