9839731923963c91f9360804bb5c423cd66f3a63
Shazzadul-Shakib <shakib1186@gmail.com>
2023-12-01 14:31:07 +0600

    skills section done

--- utils/skillIcons.tsx
+++ utils/skillIcons.tsx
@@ -1,19 +1,25 @@
-import { FaHtml5, FaCss3, FaBootstrap, FaReact } from "react-icons/fa6";
-import { IoLogoJavascript } from "react-icons/io5";
+import { FaHtml5, FaCss3, FaBootstrap, FaReact, FaNodeJs, FaNpm, FaFigma, FaGitAlt, FaGithub } from "react-icons/fa6";
+import { IoLogoFirebase, IoLogoJavascript } from "react-icons/io5";
+import { BiLogoNetlify } from "react-icons/bi";
 import {
+  SiCanva,
+  SiExpress,
+  SiMongodb,
+  SiMongoose,
   SiReactquery,
   SiReactrouter,
   SiRedux,
   SiTailwindcss,
+  SiVercel,
 } from "react-icons/si";
-import { TbBrandNextjs } from "react-icons/tb";
+import { TbBrandNextjs, TbBrandVscode } from "react-icons/tb";
 
-type FrontEndItem = {
+type SkillItem = {
   icon: JSX.Element;
   title: string;
 };
 
-export const FrontEnd: FrontEndItem[] = [
+export const FrontEnd: SkillItem[] = [
   {
     icon: <FaHtml5 className="text-2xl" />,
     title: "Html5",
@@ -53,5 +59,67 @@ export const FrontEnd: FrontEndItem[] = [
   {
     icon: <SiReactquery className="text-2xl" />,
     title: "React-query",
+  },
+];
+
+export const BackEnd: SkillItem[] = [
+  {
+    icon: <FaNodeJs className="text-2xl" />,
+    title: "Node.js",
+  },
+  {
+    icon: <SiExpress className="text-2xl" />,
+    title: "Express.js",
+  },
+  {
+    icon: <SiMongodb className="text-2xl" />,
+    title: "MongoDb",
+  },
+  {
+    icon: <SiMongoose className="text-2xl" />,
+    title: "Mongoose",
+  },
+  {
+    icon: <SiMongoose className="text-2xl" />,
+    title: "Mongoose",
+  },
+  {
+    icon: <IoLogoFirebase className="text-2xl" />,
+    title: "Firebase",
+  },
+];
+
+export const Tools: SkillItem[] = [
+  {
+    icon: <TbBrandVscode className="text-2xl" />,
+    title: "Vscode",
+  },
+  {
+    icon: <FaNpm className="text-2xl" />,
+    title: "npm",
+  },
+  {
+    icon: <FaFigma className="text-2xl" />,
+    title: "Figma",
+  },
+  {
+    icon: <FaGitAlt className="text-2xl" />,
+    title: "Git",
+  },
+  {
+    icon: <FaGithub className="text-2xl" />,
+    title: "Github",
+  },
+  {
+    icon: <SiCanva className="text-2xl" />,
+    title: "Canva",
+  },
+  {
+    icon: <SiVercel className="text-2xl" />,
+    title: "Vercel",
+  },
+  {
+    icon: <BiLogoNetlify className="text-2xl" />,
+    title: "Netlify",
   },
 ];
