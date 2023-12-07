766a91208e7a5fbe6df4e8d3e7acf08c7ede0fb3
Shazzadul-Shakib <shakib1186@gmail.com>
2023-12-07 11:24:52 +0600

    About section added

--- app/hero/page.tsx
+++ app/hero/page.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   return (
     <main
       id="hero"
-      className="flex gap-10 pt-52 bg-[url('/logoB.png')] bg-no-repeat bg-[length:300px_300px] bg-[center_right]  md:bg-[center_right_16rem] md:bg-auto md:gap-48"
+      className="flex gap-5 pt-52 bg-[url('/logoB.png')] bg-no-repeat bg-[length:300px_300px] bg-[center_right]  md:bg-[center_right_16rem] md:bg-auto md:gap-48"
     >
       <div className=" ml-4">
         <div className="flex flex-col items-center justify-center">
@@ -24,13 +24,31 @@ export default function Hero() {
           <h1 className="text-white text-3xl font-bold md:text-7xl">
             Shazzadul Islam Shakib
           </h1>
-          <h2 className=" text-gray-400 text-lg font-semibold italic my-5 md:text-xl">
+          <h2 className=" text-gray-400 text-lg font-semibold italic my-2 md:text-xl">
             Front-End Developer
           </h2>
+          <p className="text-gray-200 text-sm w-[95%] py-3 rounded-md md:w-[75%]">
+            Passionate and innovative front-end developer specializing in{" "}
+            <span className="text-[#EF403A] text-base"> React</span> , with a
+            keen eye for crafting visually appealing and{" "}
+            <span className="text-[#EF403A] text-base">
+              user-friendly interfaces
+            </span>{" "}
+            . Leveraging a strong foundation in modern web technologies, I am
+            dedicated to creating seamless,
+            <span className="text-[#EF403A] text-base">responsive</span> , and
+            performant user experiences that elevate digital landscapes. Let's
+            transform ideas into captivating web realities together.
+          </p>
+          <p className=" text-gray-200 text-sm w-[75%] mb-3 ">
+            When not working or futzing around with code, I study how to escape
+            from University. Actually
+            <span className="text-[#EF403A] text-base">for hire</span>.
+          </p>
         </div>
         <div className="flex items-center">
           <button className="bg-[#EF403A] px-4 py-2 text-white font-semibold rounded-md">
-            About Me
+            Resume
           </button>
           <HiArrowLongRight className=" text-4xl text-white -ml-2" />
         </div>
---
+++ app/ui/font.ts
@@ -0,0 +1,3 @@
+import { League_Spartan } from "next/font/google";
+
+export const League = League_Spartan({ weight: ["100" , "200" ,'300' ,'400','500','600','700','800','900'], subsets: ["latin"] });
